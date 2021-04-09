21fb384d04607e61cf8c85d85e15bae10a3659ac
A.Adegoke Jnr <voomsway@Adegokes-MacBook-Pro.local>
2021-04-09 14:59:48 +0100

    deploying to heroku

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,12 +1,4 @@
-import { Transform } from 'class-transformer';
-import { IsEmail, IsString } from 'class-validator';
-
 export class SignInDto {
-  @IsEmail()
-  @Transform((s) => String(s.value).trim().toLowerCase())
   readonly email: string;
-
-  @IsString()
-  @Transform((s) => String(s.value).trim())
   readonly password: string;
 }
--- src/auth/dto/sign-up.dto.ts
+++ src/auth/dto/sign-up.dto.ts
@@ -1,19 +1,6 @@
-import { Transform } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
-
 export class SignUpDto {
-  @IsEmail()
-  @IsNotEmpty()
-  @Transform((s) => String(s.value).trim().toLowerCase())
   readonly email: string;
-
-  @IsString()
-  @Transform((s) => String(s.value).trim())
   readonly password: string;
-
-  @IsString()
   readonly firstName: string;
-
-  @IsString()
   readonly lastName: string;
 }
