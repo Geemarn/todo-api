d2dee3fa9e422f2ac44982d84bd94bcbc2ab3d97
voomsway <unique>
2021-04-10 16:22:30 +0100

    task endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import config from './config/keys';
 import { UserModule } from './user/user.module';
-import { TaskService } from './task/task.service';
 import { TaskModule } from './task/task.module';
 
 @Module({
